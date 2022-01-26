3772aaba6cde07d3a2d3fb8b867888a6fa84a9f8
vladoja <dr.jalo2@gmail.com>
2022-01-26 09:36:44 +0100

    Adding squares to board.

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
 const initGameEngine = () => {
     const size = 100;
-    const grid = Array.prototype.fill(0, 0, size);
+    const bombsAmount = 12;
+    const grid = [];
+    const bombsArray = Array(bombsAmount).fill('bomb');
+    const emptyArray = Array(size - bombsAmount).fill('valid');
+    const gameArray = emptyArray.concat(bombsArray);
+    const shuffledArray = shuffle(gameArray);
+
+    console.log(shuffledArray);
+
+    /**
+     * Shuffles array in place with
+     * the modern version of the Fisher–Yates shuffle algorithm.
+     * @param {Array} a items An array containing the items.
+     */
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
 
 
-    function createGrid(gridElement) {
-        console.log('Creating board: ', gridElement)
+    function createBoard(gridElement) {
+        console.log('Creating board: ', gridElement);
         for (let i = 0; i < size; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.classList.add(shuffledArray[i]);
             square.id = `square-${i}`;
+            square.innerHTML = '' + i;
+
             gridElement.appendChild(square);
+            grid.push(square);
+
+            square.addEventListener('click', e => {
+                console.log(`Clicked on square ${e.target.id}`, e);
+            })
         }
+    }
+
+
+    function placeBombs() {
+        bombsArray.fill()
+        Array
+        bombsArray.fill()
+    }
+
+
+    function getNeighbors(id) {
 
     }
 
 
     return {
-        createGrid
+        createBoard
     }
 
 }
@@ -26,6 +68,6 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Dom loaded');
     const gameEngine = initGameEngine();
     const gridElement = document.querySelector('#grid');
-    gameEngine.createGrid(gridElement);
+    gameEngine.createBoard(gridElement);
 
 });
